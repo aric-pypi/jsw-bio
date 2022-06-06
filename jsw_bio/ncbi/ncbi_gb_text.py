d0934be6d5c1355eaaec5a9303d77a88c1e8de3a
feizheng <1290657123@qq.com>
2022-06-06 18:41:58 +0800

    refactor: upt base_url

--- jsw_bio/ncbi/ncbi_gb_text.py
+++ jsw_bio/ncbi/ncbi_gb_text.py
@@ -1,10 +1,12 @@
 import requests
+import jsw_nx as nx
+from .settings import *
 
 
 def ncbi_gb_text(**kwargs):
     id = kwargs.get('id')
-    res = requests.get(
-        f'https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id={id}&db=protein&report=genpept&retmode=text')
+    qs = nx.param({'id': id, 'db': 'protein', 'report': 'genpept', 'retmode': 'text'})
+    res = requests.get(f'{BASE_URL}/sviewer/viewer.fcgi?{qs}')
     if res.status_code == 200:
         return res.text
     return None
