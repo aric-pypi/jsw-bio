269315e10ef66a10490bf9425a0cd5d81fc4b608
feizheng <1290657123@qq.com>
2022-08-22 15:31:09 +0800

    feat: add entrez_download_accids

---
+++ __tests__/ncbi/test_entrez_download_accids.py
@@ -0,0 +1,14 @@
+import jsw_bio as bio
+import jsw_nx as nx
+
+
+def test_download_accids():
+    pass
+    # the 28 records
+    # res1 = bio.ncbi_download_accids(term='cas12')
+
+    # not exist
+    # bio.entrez_download_accids(term='cas15')
+
+    # assert len(res1) > 0
+    # assert len(res2) == 0
