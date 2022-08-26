c720699cba33d8a487f3924b4c9ebd2b39cff7b5
feizheng <1290657123@qq.com>
2022-08-26 13:53:45 +0800

    fix: rm loger

--- jsw_bio/ncbi/ncbi_fasta2md5.py
+++ jsw_bio/ncbi/ncbi_fasta2md5.py
@@ -6,6 +6,5 @@ def ncbi_fasta2md5(fasta_str):
     seq = ''.join(lines[1:]).strip()
     seq = seq.replace('\n', '')
 
-    print('processed seq is: ',seq)
     md5 = hashlib.md5(seq.encode('utf-8')).hexdigest()
     return md5.upper()
